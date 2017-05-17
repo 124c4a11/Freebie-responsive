2e4209308716da5713e968cce952ca07d423c399
124c4a11 <124c4a11@mail.ru>
2017-05-17 16:13:52 +0300

    gallery.js: Implementing logic for .recent-works

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,15 @@
 'use strict';
 
 import mainMenu from './modules/mainMenu';
+import gallery from './modules/gallery';
 
 $(document).ready(function() {
   if ($('.main-menu').length) {
     mainMenu.init();
+  }
+
+  if ($('.recent-works').length) {
+    gallery.init($('.recent-works'));
   }
 });
 
