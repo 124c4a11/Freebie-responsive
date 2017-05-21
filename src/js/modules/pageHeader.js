121daddf6e68a2e96eff44560a5820fac390e4bf
124c4a11 <124c4a11@mail.ru>
2017-05-21 19:21:46 +0300

    pageHeader.js: Improved logic (fix ie).

--- src/js/modules/pageHeader.js
+++ src/js/modules/pageHeader.js
@@ -1,25 +1,31 @@
 'use strict';
 
 var
-  $pageHeader = $('.page-header');
+  $pageHeader = $('.page-header'),
+  pageHeaderHeight = $pageHeader.height();
 
+// scrolling variables
 var
-  pageHeaderHeight = $pageHeader.height(),
-  previousTop = 0;
+  scrollDelta = 10,
+  previousTop = 0,
+  currentTop = 0;
 
 
-function onScroll(currentTop) {
-  if (currentTop <= previousTop) {
+function onScroll(scrollTop) {
+  currentTop = scrollTop;
+  autoHideHeader(currentTop);
+  previousTop = currentTop;
+}
 
-    // if scrolling up...
+
+function autoHideHeader(currentTop) {
+  if (previousTop - currentTop > 0) { // if scrolling up...
     if (currentTop > 0 && $pageHeader.hasClass('page-header_fixed')) {
       $pageHeader.addClass('page-header_fixed-visible');
     } else {
       $pageHeader.removeClass('page-header_fixed page-header_fixed-visible');
     }
-  } else {
-
-    // if scrolling down
+  } else if (previousTop - currentTop < 0) { // if scrolling down... (fixed ie)
     if ($pageHeader.hasClass('page-header_fixed-visible')) {
       $pageHeader.removeClass('page-header_fixed-visible');
     }
@@ -28,8 +34,6 @@ function onScroll(currentTop) {
       $pageHeader.addClass('page-header_fixed');
     }
   }
-
-  previousTop = currentTop;
 }
 
 
